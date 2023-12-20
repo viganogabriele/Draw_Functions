ce3e7da6086846f9f1703d44deb5df727f190a2d
Michele Frazzoni <frazzonimichele@gmail.com>
2023-12-20 11:55:01 +0100

    Implementato metodo tangente

--- Funzione.cs
+++ Funzione.cs
@@ -22,12 +22,12 @@ namespace DisegnoFunzione
         }
         public bool Trovato(double xA, double xB, double epsilon)
         {
-            return (xB - xA) < epsilon;
+            return Math.Abs(xB - xA) < epsilon;
         }
         public double Bisezione(double xA, double xB, double epsilon)
         {
             double m = (xA + xB) / 2;
-            while (Trovato(xA, xB, epsilon) == true)
+            while (Trovato(xA, xB, epsilon) == true || passaggi[0] > 1000)
             {
                 if (m > xA)
                 {
@@ -44,7 +44,19 @@ namespace DisegnoFunzione
         }
         public double Tangente(double xA, double xB, double epsilon)
         {
-
+            double x0;
+            double x1 = xA;
+            double m;
+            double q;
+            double h = 0.00000000000001;
+            while (Trovato(x0, x1, epsilon) == true || passaggi[1] > 1000)
+            {
+                x0 = x1;
+                m = (Y(x0) - Y(x0+h)) / h;
+                q = Y(x0) - m * x0;
+                x1 = - q / m;
+                passaggi[1]++;
+            }
         }
     }
 }
